b57b49c44334a700a1deeeda94f02486fd0aaf61
Sam Mortimer <sam@mortimer.me.uk>
2013-07-23 15:39:39 -0700

    (WIP) Test hack for jf call audio issue
    Change-Id: I4725c5ba5419a05a2bfc06ea0eec2243f197b9ae

--- alsa_sound/ALSADevice.cpp
+++ alsa_sound/ALSADevice.cpp
@@ -1823,7 +1823,11 @@ char* ALSADevice::getUCMDevice(uint32_t devices, int input, char *rxDevice)
                                 SND_USE_CASE_DEV_VOC_EARPIECE); /* Voice HANDSET RX */
                 }
             } else {
+#if 0
                 return strdup(SND_USE_CASE_DEV_EARPIECE); /* HANDSET RX */
+#else
+                return strdup(SND_USE_CASE_DEV_VOC_EARPIECE);
+#endif
             }
         } else if (devices & AudioSystem::DEVICE_OUT_SPEAKER) {
 #ifdef SEPERATED_VOICE_SPEAKER
