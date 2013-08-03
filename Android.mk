3e4f9a3e2d5acde892a21f46ca1c0d29210d447c
dhacker29 <davidhackerdvm@gmail.com>
2013-08-02 21:48:56 -0500

    Use -Wno-error to prevent compile errors
    I am not sure if it is a difference in the 3.0 kernel or what
    but without this I get tons of error messages on signedness
    
    Change-Id: Ia20a6470a41c9b9fa418ee3d173c78ba82b4ffe2

--- Android.mk
+++ Android.mk
@@ -1,6 +1,8 @@
 ifneq ($(filter msm8960 msm8660 msm7x30 msm8226 msm8x26 msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
 ifeq ($(TARGET_QCOM_AUDIO_VARIANT),caf)
 
+common_flags += -Wno-error
+
 MY_LOCAL_PATH := $(call my-dir)
 
 #ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
