6f695321665e7e2e6243f046a5b822855422a4c3
Arne Coucheron <arco68@gmail.com>
2013-09-02 07:18:59 +0200

    Revert "fmradio: enable STE radio"
    This reverts commit 99dfda5b9d480f2013889dfe07939c1060094235.
    
    Change-Id: I0e44c7fc49273272ef5304034d009ea47ee7f961

--- alsa_sound/AudioHardwareALSA.cpp
+++ alsa_sound/AudioHardwareALSA.cpp
@@ -952,7 +952,7 @@ status_t AudioHardwareALSA::doRouting(int device)
     if(device)
         mALSADevice->mCurDevice = device;
     if ((device == AudioSystem::DEVICE_IN_VOICE_CALL)
-#if defined(QCOM_FM_ENABLED) || defined(STE_FM)
+#ifdef QCOM_FM_ENABLED
         || (device == AudioSystem::DEVICE_IN_FM_RX)
         || (device == AudioSystem::DEVICE_IN_FM_RX_A2DP)
 #endif
@@ -1764,7 +1764,7 @@ AudioHardwareALSA::openInputStream(uint32_t devices,
                         }
                     }
                 }
-#if defined(QCOM_FM_ENABLED) || defined(STE_FM)
+#ifdef QCOM_FM_ENABLED
             } else if((devices == AudioSystem::DEVICE_IN_FM_RX)) {
                 strlcpy(alsa_handle.useCase, SND_USE_CASE_MOD_CAPTURE_FM, sizeof(alsa_handle.useCase));
             } else if(devices == AudioSystem::DEVICE_IN_FM_RX_A2DP) {
@@ -1817,7 +1817,7 @@ AudioHardwareALSA::openInputStream(uint32_t devices,
                         }
                     }
                 }
-#if defined(QCOM_FM_ENABLED) || defined(STE_FM)
+#ifdef QCOM_FM_ENABLED
             } else if(devices == AudioSystem::DEVICE_IN_FM_RX) {
                 strlcpy(alsa_handle.useCase, SND_USE_CASE_VERB_FM_REC, sizeof(alsa_handle.useCase));
             } else if (devices == AudioSystem::DEVICE_IN_FM_RX_A2DP) {
