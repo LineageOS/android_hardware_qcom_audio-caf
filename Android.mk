a85c0c4aa912aeebde928db6d356c31db85467d3
David Ferguson <ferguson.david@gmail.com>
2013-11-19 18:27:24 -0500

    audio-caf: resurrect msm8660 audio
      * based on http://review.cyanogenmod.org/#/c/53023/ which was
        based on http://review.cyanogenmod.org/#/c/52982/
    
    Change-Id: I7369d744282f384d6a9ed78f1b14efad7eb0d567

--- Android.mk
+++ Android.mk
@@ -1,11 +1,20 @@
-ifneq ($(filter msm8960 msm8226 msm8x26 msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8960 msm8660 msm8226 msm8x26 msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
+
+ifeq ($(TARGET_QCOM_AUDIO_VARIANT),caf)
 
 MY_LOCAL_PATH := $(call my-dir)
 
 ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
-include $(MY_LOCAL_PATH)/legacy/Android.mk
+    include $(MY_LOCAL_PATH)/legacy/Android.mk
 else
-include $(MY_LOCAL_PATH)/hal/Android.mk
+    ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
+        include $(MY_LOCAL_PATH)/legacy/msm8660/Android.mk
+        include $(MY_LOCAL_PATH)/legacy/mm-audio/Android.mk
+    else
+        include $(MY_LOCAL_PATH)/hal/Android.mk
+    endif
+endif
+
 endif
 
 endif
