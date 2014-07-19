d9d06bb134899efb7fb52125599c8570ba7aae0f
Steve Kondik <shade@chemlab.org>
2014-07-19 15:53:00 -0700

    audio: Fix makefile for legacy targets
     * Legacy targets were using the wrong policy HAL. Fixit.
    
    Change-Id: Id3d37d5d96bae8303b05082a459f2cbcba53c5d1

--- Android.mk
+++ Android.mk
@@ -10,13 +10,13 @@ ifneq ($(filter mpq8092,$(TARGET_BOARD_PLATFORM)),)
 include $(MY_LOCAL_PATH)/hal_mpq/Android.mk
 else
 include $(MY_LOCAL_PATH)/hal/Android.mk
+include $(MY_LOCAL_PATH)/policy_hal/Android.mk
+include $(MY_LOCAL_PATH)/audiod/Android.mk
+include $(MY_LOCAL_PATH)/post_proc/Android.mk
 endif
 include $(MY_LOCAL_PATH)/voice_processing/Android.mk
 include $(MY_LOCAL_PATH)/mm-audio/Android.mk
-include $(MY_LOCAL_PATH)/policy_hal/Android.mk
 include $(MY_LOCAL_PATH)/visualizer/Android.mk
-include $(MY_LOCAL_PATH)/audiod/Android.mk
-include $(MY_LOCAL_PATH)/post_proc/Android.mk
 endif
 
 endif
